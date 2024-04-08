bf1580ea81bd861ef09aebc152f90967e1390a13
Marco Volle <mavolle@students.uni-mainz.de>
2024-04-08 11:35:05 +0200

    added other mdvne projects to copy script

--- scripts/gips-ex-to-iflye.bat
+++ scripts/gips-ex-to-iflye.bat
@@ -8,4 +8,10 @@ set MDVNE_PROJECT_NAME=org.emoflon.gips.gipsl.examples.mdvne
 rmdir ..\%MDVNE_PROJECT_NAME%\src-gen /s /q
 xcopy ..\..\gips-examples\%MDVNE_PROJECT_NAME%\src-gen ..\%MDVNE_PROJECT_NAME%\src-gen\ /e
 
+rmdir ..\%MDVNE_PROJECT_NAME%.migration\src-gen /s /q
+xcopy ..\..\gips-examples\%MDVNE_PROJECT_NAME%\src-gen ..\%MDVNE_PROJECT_NAME%.migration\src-gen\ /e
+
+rmdir ..\%MDVNE_PROJECT_NAME%.seq\src-gen /s /q
+xcopy ..\..\gips-examples\%MDVNE_PROJECT_NAME%\src-gen ..\%MDVNE_PROJECT_NAME%.seq\src-gen\ /e
+
 endlocal
